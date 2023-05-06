c991698ce7748358ec4c24d069d91ad543e8f7c5
Liyan Zhao <return65535@qq.com>
2023-05-06 13:05:54 +0800

    fix: #10

---
+++ src/main/java/com/github/zly2006/enclosure/mixin/MixinCrossBowItem.java
@@ -0,0 +1,31 @@
+package com.github.zly2006.enclosure.mixin;
+
+import com.github.zly2006.enclosure.ServerMain;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import static com.github.zly2006.enclosure.utils.Permission.SHOOT;
+
+@Mixin(CrossbowItem.class)
+public class MixinCrossBowItem {
+    @Inject(method = "use", locals = LocalCapture.CAPTURE_FAILSOFT, at = @At(value = "INVOKE",target = "Lnet/minecraft/item/CrossbowItem;shootAll(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/item/ItemStack;FF)V"), cancellable = true)
+    private void onShoot(World world, PlayerEntity user, Hand hand, CallbackInfoReturnable<TypedActionResult<ItemStack>> cir, ItemStack itemStack) {
+        if (user instanceof ServerPlayerEntity player) {
+            if (!ServerMain.INSTANCE.checkPermission(player, SHOOT, player.getBlockPos())) {
+                player.sendMessage(SHOOT.getNoPermissionMsg(player));
+                player.currentScreenHandler.syncState();  // update player's inventory
+                cir.setReturnValue(TypedActionResult.fail(itemStack));
+            }
+        }
+    }
+}
