36ebc5dd0729a29c8cd22e78963ff209712b208f
Liyan Zhao <return65535@qq.com>
2023-06-02 23:16:30 +0800

    feat: 1.20-rc1

--- src/main/kotlin/com/github/zly2006/enclosure/client/ClientConfigScreen.kt
+++ src/main/kotlin/com/github/zly2006/enclosure/client/ClientConfigScreen.kt
@@ -1,12 +1,12 @@
 package com.github.zly2006.enclosure.client
 
+import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
-import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.text.Text
 
 class ClientConfigScreen: Screen(Text.literal("Enclosure Config")) {
-    override fun render(matrices: MatrixStack?, mouseX: Int, mouseY: Int, delta: Float) {
-        super.render(matrices, mouseX, mouseY, delta)
+    override fun render(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
+        super.render(context, mouseX, mouseY, delta)
     }
 
     override fun init() {
