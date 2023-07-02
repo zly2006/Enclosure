0183a6767454ffa130e73b102a1f6bd44534d31a
Liyan Zhao <return65535@qq.com>
2023-07-02 08:50:54 +0800

    fix: feed animal

--- src/main/java/com/github/zly2006/enclosure/mixin/MixinAnimalEntity.java
+++ src/main/java/com/github/zly2006/enclosure/mixin/MixinAnimalEntity.java
@@ -6,14 +6,14 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.github.zly2006.enclosure.utils.Permission.FEED_ANIMAL;
 
@@ -23,15 +23,23 @@ public abstract class MixinAnimalEntity extends Entity {
         super(type, world);
     }
 
-    @Inject(at = @At("HEAD"), method = "eat", cancellable = true)
-    private void onEating(PlayerEntity player, Hand hand, ItemStack stack, CallbackInfo ci) {
+    @Inject(
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/passive/AnimalEntity;getBreedingAge()I",
+                    shift = At.Shift.BEFORE
+            ),
+            method = "interactMob",
+            cancellable = true
+    )
+    private void onEating(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         if (getWorld().isClient) {
             return;
         }
-        EnclosureArea area = ServerMain.INSTANCE.getAllEnclosures((ServerWorld) this.getWorld()).getArea(getBlockPos());
-        if (area != null && !area.areaOf(getBlockPos()).hasPubPerm(FEED_ANIMAL)) {
+        EnclosureArea area = ServerMain.INSTANCE.getSmallestEnclosure((ServerWorld) this.getWorld(), getBlockPos());
+        if (area != null && !area.hasPubPerm(FEED_ANIMAL)) {
             player.sendMessage(FEED_ANIMAL.getNoPermissionMsg(player));
-            ci.cancel();
+            cir.setReturnValue(ActionResult.FAIL);
         }
     }
 }
