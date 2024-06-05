c88c5d2e95d6fe684dacb7c99a797b166a705c89
Liyan Zhao <return65535@qq.com>
2024-06-06 02:43:49 +0800

    fix: #40

--- src/main/java/com/github/zly2006/enclosure/mixin/workaround/MixinRegistrySyncManager.java
+++ src/main/java/com/github/zly2006/enclosure/mixin/workaround/MixinRegistrySyncManager.java
@@ -2,24 +2,22 @@ package com.github.zly2006.enclosure.mixin.workaround;
 
 import com.github.zly2006.enclosure.gui.EnclosureScreenHandler;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import net.fabricmc.fabric.impl.registry.sync.RegistrySyncManager;
+import net.fabricmc.fabric.impl.registry.sync.packet.DirectRegistryPacketHandler;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import java.util.Map;
 
 @SuppressWarnings("UnstableApiUsage")
-@Mixin(value = RegistrySyncManager.SyncConfigurationTask.class, remap = false)
+@Mixin(value = DirectRegistryPacketHandler.class, remap = false)
 public class MixinRegistrySyncManager {
-    @ModifyArg(
+    @ModifyVariable(
             method = "sendPacket",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/fabricmc/fabric/impl/registry/sync/packet/RegistryPacketHandler;sendPacket(Ljava/util/function/Consumer;Ljava/util/Map;)V"
-            )
+            at = @At("HEAD"),
+            argsOnly = true
     )
     private Map<Identifier, Object2IntMap<Identifier>> modifyIds(Map<Identifier, Object2IntMap<Identifier>> ids) {
         Object2IntMap<Identifier> map = ids.get(Registries.SCREEN_HANDLER.getKey().getValue());
