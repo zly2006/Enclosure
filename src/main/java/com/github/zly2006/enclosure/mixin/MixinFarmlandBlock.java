bd8c2244268ec487f34ba7a34d6f4070ce0e7010
Liyan Zhao <return65535@qq.com>
2024-07-12 23:52:38 +0800

    fix: #43

--- src/main/java/com/github/zly2006/enclosure/mixin/MixinFarmlandBlock.java
+++ src/main/java/com/github/zly2006/enclosure/mixin/MixinFarmlandBlock.java
@@ -15,8 +15,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FarmlandBlock.class)
 public class MixinFarmlandBlock {
-    @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
-    private void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
+    @Inject(method = "setToDirt", at = @At("HEAD"), cancellable = true)
+    private static void onLandedUpon(Entity entity, BlockState state, World world, BlockPos pos, CallbackInfo ci) {
         if (entity instanceof PlayerEntity player) {
             if (!ServerMain.INSTANCE.checkPermission(world, pos, player, Permission.FARMLAND_DESTROY)) {
                 entity.sendMessage(Permission.FARMLAND_DESTROY.getNoPermissionMsg(player));
